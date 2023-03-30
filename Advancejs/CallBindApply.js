bfbbe61752bf2699ea2a1c90687d51832c68e7f9
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-03-30 16:36:38 +0600

    new

--- Advancejs/CallBindApply.js
+++ Advancejs/CallBindApply.js
@@ -1,69 +1,69 @@
-const person = {
-    fullName: function () {
-        return this.firstName + " " + this.lastName;
-    }
-}
+// const person = {
+//     fullName: function () {
+//         return this.firstName + " " + this.lastName;
+//     }
+// }
 
-const person1 = {
-    firstName: "John",
-    lastName: "Doe"
-}
-const person2 = {
-    firstName: "Mary",
-    lastName: "Doe"
-}
+// const person1 = {
+//     firstName: "John",
+//     lastName: "Doe"
+// }
+// const person2 = {
+//     firstName: "Mary",
+//     lastName: "Doe"
+// }
 
-// This will return "John Doe":
-const result= person.fullName.call(person2);
-console.log(result);
-
-
-//pass new vlaue 
-const person = {
-    fullName: function (a) {
-        return this.firstName + " " + this.lastName+a;
-    }
-}
-
-const person1 = {
-    firstName: "John",
-    lastName: "Doe"
-}
-const person2 = {
-    firstName: "Mary",
-    lastName: "Doe"
-}
-
-var hand="handsam"
-
-// This will return "John Doe":
-const result = person.fullName.call(person2,hand);
-console.log(result);
-pass new vlaue 
+// // This will return "John Doe":
+// const result= person.fullName.call(person2);
+// console.log(result);
 
 
+// //pass new vlaue 
+// const person = {
+//     fullName: function (a) {
+//         return this.firstName + " " + this.lastName+a;
+//     }
+// }
 
-const person = {
-    fullName: function (a,b) {
-        return this.firstName + " " + this.lastName+a+b;
-    }
-}
+// const person1 = {
+//     firstName: "John",
+//     lastName: "Doe"
+// }
+// const person2 = {
+//     firstName: "Mary",
+//     lastName: "Doe"
+// }
 
-const person1 = {
-    firstName: "John",
-    lastName: "Doe"
-}
-const person2 = {
-    firstName: "Mary",
-    lastName: "Doe"
-}
+// var hand="handsam"
+
+// // This will return "John Doe":
+// const result = person.fullName.call(person2,hand);
+// console.log(result);
+// pass new vlaue 
 
 
-var a="A"
-var b="B"
 
-var hand=[a,b]
+// const person = {
+//     fullName: function (a,b) {
+//         return this.firstName + " " + this.lastName+a+b;
+//     }
+// }
 
-// This will return "John Doe":
-const result = person.fullName.apply(person2,hand);
-console.log(result);
+// const person1 = {
+//     firstName: "John",
+//     lastName: "Doe"
+// }
+// const person2 = {
+//     firstName: "Mary",
+//     lastName: "Doe"
+// }
+
+
+// var a="A"
+// var b="B"
+
+// var hand=[a,b]
+
+// // This will return "John Doe":
+// const result = person.fullName.apply(person2,hand);
+// console.log(result);
---
+++ Advancejs/new.js
@@ -0,0 +1,9 @@
+function Person(name,age){
+    this.name=name;
+    this.age=age
+    console.log(name+age);
+
+
+}
+var sakib2=new Person("mozahid1",34)
+var sakib2=new Person("mozahid2",354)
